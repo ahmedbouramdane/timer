7fad7d704f5475417de79c9b7cd894544a5f68a4
ahmedbouramdane <Ahmedbourmdane77@gmail.com>
2025-09-13 22:00:46 +0100

    2nd update

--- js/main.js
+++ js/main.js
@@ -171,13 +171,13 @@ function applyResponsiveDesign() {
         pauseBtn.classList.remove('btn-lg', 'me-3');
         pauseBtn.classList.add('btn-md', 'mb-2', 'w-100');
         resetBtn.classList.remove('btn-lg');
-        resetBtn.classList.add('btn-md', 'w-100');
+        resetBtn.classList.add('btn-md', 'w-100', 'mb-2');
     } else {
         startBtn.classList.remove('btn-md', 'mb-2', 'w-100');
         startBtn.classList.add('btn-lg', 'me-3');
         pauseBtn.classList.remove('btn-md', 'mb-2', 'w-100');
         pauseBtn.classList.add('btn-lg', 'me-3');
-        resetBtn.classList.remove('btn-md', 'w-100');
+        resetBtn.classList.remove('btn-md', 'w-100', 'mb-2');
         resetBtn.classList.add('btn-lg');       
     } 
     // login full width on small screens
@@ -192,19 +192,59 @@ function applyResponsiveDesign() {
     const logoContainer = document.querySelector('.high-school-logo');
     if (window.innerWidth < 576) {
         logoContainer.style.height = '70px';
+        logoContainer.children[0].classList.remove('w-25');
+        logoContainer.children[0].classList.add('w-100');
+        logoContainer.children[1].classList.remove('w-25');
+        logoContainer.children[1].classList.add('w-100');
     } else {
         logoContainer.style.height = 'auto';
     }
-    // make container height free on small screens
+    
+
+    // small the gap class on the nav bar on small screens
+    const navBar = document.querySelector('nav .navbar-nav');
     if (window.innerWidth < 576) {
-        container.style.height = 'auto';
+        navBar.classList.remove('gap-5');
+        navBar.classList.add('gap-1');
     } else {
-        const windowHeight = window.innerHeight;
-        container.style.height = `${windowHeight - nav.offsetHeight -footer.offsetHeight}px`;
+        navBar.classList.remove('gap-1');
+        navBar.classList.add('gap-5');        
     }
-        
+
+    // add font awsome icons to the nav links
+    const navLinks = document.querySelectorAll('nav .nav-link');
+    navLinks[0].innerHTML = `<i class="fas fa-home me-2"></i> Home`;
+    navLinks[1].innerHTML = `<i class="fas fa-info me-2"></i> About us`;
+    navLinks[2].innerHTML = `<i class="fas fa-envelope me-2"></i> Contact us`;
+
+    // .set-time flex to column on small screens
+    const setTimeDiv = document.querySelector('.set-time');
+    if (window.innerWidth < 576) {
+        setTimeDiv.classList.remove('flex-row', 'justify-content-evenly');
+        setTimeDiv.classList.add('flex-column', 'align-items-center', 'gap-3');
+    }
+
+    // footer items flex to column on small screens
+    const footerDiv = document.querySelector('footer');
+    if (window.innerWidth < 576) {
+        footerDiv.classList.remove('flex-row', 'justify-content-between');
+        footerDiv.classList.add('flex-column', 'align-items-center', 'gap-2');
+    }
 }
+
 applyResponsiveDesign();
 window.addEventListener('resize', applyResponsiveDesign);   
 // onload 
 window.addEventListener('load', applyResponsiveDesign);
+
+//****************** */ End - Responsive Design ************************* 
+
+// flatpickr settings in android and mobile screen also
+flatpickr("#time-flatpickr", {
+    enableTime: true,
+    enableSeconds: true,
+    noCalendar: true,
+    dateFormat: "H:i:S",
+    time_24hr: true,
+    defaultDate: "00:00:00"
+});
